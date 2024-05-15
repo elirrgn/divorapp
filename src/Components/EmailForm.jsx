876a8891811546fbcebf048ef51bd5e28c79e9d1
elirrgn <elir.arge@gmail.com>
2024-05-15 21:25:20 +0200

    color fixing, home-contacts

--- src/Components/EmailForm.jsx
+++ src/Components/EmailForm.jsx
@@ -2,16 +2,16 @@ function EmailForm() {
     return(
         <>
             <form className="home-contact-form">
-                <label className="home-contact-form-label" for="firstName">First Name:</label>
+                <label className="home-contact-form-label" for="firstName">Name:</label>
                 <input className="home-contact-form-input" type="text" id="firstName" name="firstName" required />
                 <br/>
             
-                <label className="home-contact-form-label" for="email">Email Address:</label>
+                <label className="home-contact-form-label" for="email">Email:</label>
                 <input className="home-contact-form-input" type="email" id="email" name="email" required />
                 <br/>
                 
                 <label className="home-contact-form-label" for="message">Message:</label>
-                <textarea className="home-contact-form-message" id="message" name="message" required></textarea>
+                <textarea className="home-contact-form-input home-contact-form-message" id="message" name="message" placeholder="Type your message... " required></textarea>
                 <br/>
                 
                 <input className="home-contact-form-send" type="submit" value="Send" />
